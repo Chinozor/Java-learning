3fa55cbe7afc97058014c21fadbd8093e9ad4320
Chinozor <yrikalexsin@gmail.com>
2024-10-22 19:40:29 +0300

    hello world

---
+++ src/main/java/p93n6/p93n6.java
@@ -0,0 +1,10 @@
+package p93n6;
+
+public class p93n6 {
+    public static void main(String[] args) {
+        System.out.println(storage("carrot"));
+    }
+    static int storage(String rabbitName){
+        return rabbitName.length()*2;
+    }
+}
