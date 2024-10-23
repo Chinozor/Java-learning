b8ac3360abac99ddc259cee5fd737547460ff87b
Chinozor <yrikalexsin@gmail.com>
2024-10-23 18:55:35 +0300

    my system

---
+++ src/main/java/p93n10/p93n10.java
@@ -0,0 +1,9 @@
+package p93n10;
+
+public class p93n10 {
+    public static void main(String[] args) {
+        System.out.println(System.getProperty("user.name"));
+        System.out.println(System.getProperty("java.vm.specification.vendor"));
+        System.out.println(System.getProperty("os.arch"));
+    }
+}
