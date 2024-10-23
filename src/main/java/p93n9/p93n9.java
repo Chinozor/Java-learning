5e644a656fc85360da95d8943e6f10d82eb6807e
Chinozor <yrikalexsin@gmail.com>
2024-10-23 18:45:37 +0300

    n9

--- src/main/java/p93n9/p93n9.java
+++ src/main/java/p93n9/p93n9.java
@@ -2,7 +2,7 @@ package p93n9;
 
 public class p93n9 {
     public static void main(String[] args) {
-        Integer i = 9;
-        System.out.println(i);
+        Integer number = 9;
+        System.out.println(number);
     }
 }
