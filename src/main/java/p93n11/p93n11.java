89b80990c6e8e6924372545c36e809ca6b499157
Chinozor <yrikalexsin@gmail.com>
2024-10-23 19:27:36 +0300

    rainbow

---
+++ src/main/java/p93n11/p93n11.java
@@ -0,0 +1,11 @@
+package p93n11;
+
+public class p93n11 {
+    public static void main(String[] args) {
+        changeTheHueOfTheColor(17);
+    }
+    int anIntegerRepresentingColors;
+    static void changeTheHueOfTheColor(int newHue){
+        System.out.println(newHue);
+    }
+}
