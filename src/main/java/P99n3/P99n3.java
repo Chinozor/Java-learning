f438519e00be72797add5c3ea9a59c981b06dc93
Chinozor <yrikalexsin@gmail.com>
2024-10-25 00:14:14 +0300

    Reactor capacity

---
+++ src/main/java/P99n3/P99n3.java
@@ -0,0 +1,16 @@
+package P99n3;
+class LevelCapacity {
+    float watt;
+}
+public class P99n3 {
+    static void capacity(LevelCapacity n){
+        n.watt = 1300.0F;
+    }
+    public static void main(String[] args) {
+        LevelCapacity max = new LevelCapacity();
+        max.watt = 1200.0F;
+        System.out.println("Capacity level = " + max.watt + " MW");
+        capacity(max);
+        System.out.println("Capacity level = " + max.watt + " MW");
+    }
+}
