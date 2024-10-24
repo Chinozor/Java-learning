60e38bfd05f4823ac80ac9b5e58edfdde1db601c
Chinozor <yrikalexsin@gmail.com>
2024-10-25 00:12:13 +0300

    Tank calibre

--- src/main/java/p93n6/p93n6.java
+++ src/main/java/p93n6/p93n6.java
@@ -2,7 +2,7 @@ package p93n6;
 
 public class p93n6 {
     public static void main(String[] args) {
-        System.out.println(storage("carrot"));
+        System.out.println(storage("carrot"));/**вывод  морковки**/
     }
     static int storage(String rabbitName){
         return rabbitName.length()*2;
---
+++ src/main/java/p96n1/P96n1.java
@@ -0,0 +1,11 @@
+package p96n1;
+import java.util.*;
+import static java.sql.DriverManager.println;
+
+public class P96n1 {
+    public static void main(String[] args) {
+        println("Hi:)");
+        System.out.println("Radiation warning");
+
+    }
+}
