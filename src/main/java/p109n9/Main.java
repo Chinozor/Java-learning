17b2a31052c5ae8e1c8345ced826b735737d2501
Chinozor <yrikalexsin@gmail.com>
2024-10-27 17:20:14 +0200

    max min float and double

---
+++ src/main/java/p109n9/Main.java
@@ -0,0 +1,10 @@
+package p109n9;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(Float.MAX_VALUE);
+        System.out.println(Float.MIN_VALUE);
+        System.out.println(Double.MAX_VALUE);
+        System.out.println(Double.MIN_VALUE);
+    }
+}
