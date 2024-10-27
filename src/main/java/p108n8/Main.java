db10ffabc896708e7bb0622ab15855abe1470097
Chinozor <yrikalexsin@gmail.com>
2024-10-27 17:02:22 +0200

    16x8

---
+++ src/main/java/p108n8/Main.java
@@ -0,0 +1,10 @@
+package p108n8;
+
+public class Main {
+    public static void main(String[] args) {
+        long l = 0x20fL;
+        long l1 =0165L;
+        System.out.println(Long.toBinaryString(l));
+        System.out.println(Long.toBinaryString(l1));
+    }
+}
