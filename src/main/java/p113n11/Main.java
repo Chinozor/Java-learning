ea57e26f254952fa57b2bb858b8ad7e22e0afb88
Chinozor <yrikalexsin@gmail.com>
2024-10-28 11:03:54 +0200

    /2

---
+++ src/main/java/p113n11/Main.java
@@ -0,0 +1,14 @@
+package p113n11;
+
+public class Main {
+    public static void main(String[] args) {
+        int i = 0x55555555;
+        System.out.println(i);
+        System.out.println(Integer.toBinaryString(i));
+        while (i != 1){
+            int i1 = i >> 1;
+            System.out.println(i1);
+            i = i1;
+        }
+    }
+}
