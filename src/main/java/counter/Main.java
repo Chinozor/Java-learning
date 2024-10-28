2a217285bff9f1afd8b0817f97bbc6be201ac864
Chinozor <yrikalexsin@gmail.com>
2024-10-28 19:41:42 +0300

    counter

---
+++ src/main/java/counter/Main.java
@@ -0,0 +1,21 @@
+package counter;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 3;
+        int counter = 0;
+        int mainCounter = 0;
+        for (int j = 1; j <= number ; j++) {
+            for (int i = 1; i <= j; i++) {
+                if (j % i == 0) {
+                counter++;
+            }
+            if (counter > mainCounter){
+                mainCounter = counter;
+            }
+        }
+            counter = 0;
+        }
+        System.out.println(mainCounter);
+    }
+}
