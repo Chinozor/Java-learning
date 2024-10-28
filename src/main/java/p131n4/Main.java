6a2f8db6b45f6d6e7299b86cc69c8d5c65117d07
Chinozor <yrikalexsin@gmail.com>
2024-10-28 17:22:30 +0300

    simple

--- src/main/java/p131n4/Main.java
+++ src/main/java/p131n4/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         int number = 29;
         int i2 = 0;
-        //float number1 = 1.00f;
         for (int i = 2; i < number; i++) {
             for (int j = 2; number %  i == 0; j++){
                  i2 = i;
