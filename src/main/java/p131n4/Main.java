071a7f9b4c2bd8e3ae0cf779d3548ab693e770c4
Chinozor <yrikalexsin@gmail.com>
2024-10-28 17:21:55 +0300

    simple

---
+++ src/main/java/p131n4/Main.java
@@ -0,0 +1,21 @@
+package p131n4;
+
+public class Main {
+    public static void main(String[] args) {
+        int number = 29;
+        int i2 = 0;
+        //float number1 = 1.00f;
+        for (int i = 2; i < number; i++) {
+            for (int j = 2; number %  i == 0; j++){
+                 i2 = i;
+                 if (i2 != 0)
+                     break;
+            }
+        }
+        if (i2 == 0)
+            System.out.println("Простое");
+        else
+            System.out.println("Составное");
+    }
+
+}
