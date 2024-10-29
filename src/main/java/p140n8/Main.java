611c114836c10e544d9e414e58b972db42888986
Chinozor <yrikalexsin@gmail.com>
2024-10-30 00:07:28 +0300

    switch

---
+++ src/main/java/p140n8/Main.java
@@ -0,0 +1,49 @@
+package p140n8;
+
+public class Main {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            switch (i){
+                case 0:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 1:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 2:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 3:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 4:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 5:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 6:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 7:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 8:
+                    System.out.println("i = " + i);
+            }
+            switch (i){
+                case 9:
+                    System.out.println("i = " + i);
+            }
+
+        }
+    }
+}
