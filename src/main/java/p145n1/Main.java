4914ccbceefb2d26e61f011b12df1bf7ce172cc4
Chinozor <yrikalexsin@gmail.com>
2024-10-30 22:04:20 +0300

    tasks

---
+++ src/main/java/p145n1/Main.java
@@ -0,0 +1,12 @@
+package p145n1;
+
+public class Main {
+    public static void main(String[] args) {
+        Name name = new Name();
+        System.out.println(name.s);
+
+    }
+}
+class Name{
+    String s;
+}
