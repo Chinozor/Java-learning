5151781ab80091a85681c7a9c112ea318573c11a
Chinozor <yrikalexsin@gmail.com>
2024-10-31 14:54:06 +0300

    finalize

--- src/main/java/p161n10/Main.java
+++ src/main/java/p161n10/Main.java
@@ -2,21 +2,23 @@ package p161n10;
 
 public class Main {
     public static void main(String[] args) {
-        Tank ship = new Tank();
-        ship.tank(2);
+        Tank t1 = new Tank(1);
+        t1 = new Tank(2);
         System.gc();
     }
 }
-class Tank{
-    int i = 1;
-    public void tank(Integer integer){
-        if( integer != 1){
-            i++;
-        }
+class Tank{ ;
+    Integer counter = 1;
+    Tank(int integer){
+        System.out.println("Корабль");
+        counter = integer;
+
     }
+    @Override
     protected void finalize(){
-        if (i == 1){
-            System.out.println("Error");
+        if (counter == 1){
+            counter++;
         }
+        System.out.println("Error" + counter);
     }
 }
