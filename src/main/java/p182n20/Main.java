cc5fc4efc8cfbb6adc203b58f80a162c76275d0c
Chinozor <yrikalexsin@gmail.com>
2024-11-01 13:06:56 +0300

    ?

---
+++ src/main/java/p182n20/Main.java
@@ -0,0 +1,9 @@
+package p182n20;
+
+public class Main {
+    public static void main(String... strings) {
+       for(String s : strings)
+           System.out.print(s + " ");
+        System.out.println();
+    }
+}
