0fe2629e623423d225a652c7b2bad82e3e0c419c
Chinozor <yrikalexsin@gmail.com>
2024-11-05 22:16:31 +0300

    protected

---
+++ src/main/java/p225n16/Amphibian.java
@@ -0,0 +1,10 @@
+package p225n16;
+
+public class Amphibian {
+    void voice(){
+        System.out.println("aaa");
+    }
+    static void move(Amphibian amphibian){
+        System.out.println("walk");
+    }
+}
---
+++ src/main/java/p225n16/Frog.java
@@ -0,0 +1,8 @@
+package p225n16;
+
+public class Frog extends Amphibian{
+    public static void main(String[] args) {
+        Frog qwa = new Frog();
+        Amphibian.move(qwa);
+    }
+}
