323aa65dfaf789d08ae6500a381b31379dc363aa
Chinozor <yrikalexsin@gmail.com>
2024-11-07 20:15:13 +0300

    Расширяемость

--- src/main/java/p247n7/Music3.java
+++ src/main/java/p247n7/Music3.java
@@ -1,6 +1,21 @@
 package p247n7;
 
+import java.util.Random;
+
 public class Music3 {
+    static Random random =  new Random();
+    static Instrument randomInstrument(){
+        switch (random.nextInt(6)){
+            default:
+            case 0: return new Brass();
+            case 1: return  new Percussion();
+            case 2: return new Piano();
+            case 3: return new Stringed();
+            case 4: return new Wind();
+            case 5: return new Woodwind();
+        }
+
+    }
     public static void tune(Instrument instrument){
         instrument.play(Note.B_FLAT);
     }
@@ -10,14 +25,11 @@ public class Music3 {
     }
 
     public static void main(String[] args) {
-        Instrument[] orchestra = {
-                new Wind(),
-                new Percussion(),
-                new Stringed(),
-                new Brass(),
-                new Woodwind(),
-                new Piano()
-        };
+        Instrument[] orchestra = new Instrument[7];
+        for (int i = 0; i < orchestra.length; i++) {
+            orchestra[i] = randomInstrument();
+
+        }
         tuneALL(orchestra);
         for(Instrument i: orchestra)
             System.out.println(i);
