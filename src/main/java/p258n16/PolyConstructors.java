b39df17531055be1e53fa4dade5d168aa18c2841
Chinozor <yrikalexsin@gmail.com>
2024-11-11 16:04:53 +0300

    p258n16

---
+++ src/main/java/p258n16/PolyConstructors.java
@@ -0,0 +1,38 @@
+package p258n16;
+class Glyph{
+    void  draw(){
+        System.out.println("Glyph.draw()");
+    }
+    Glyph(){
+        System.out.println("Glyph() перед вызовом draw()");
+        draw();
+        System.out.println("Glyph() после вызова draw()");
+    }
+}
+class RoundGlyph extends Glyph{
+    private int radius = 1;
+    RoundGlyph(int r){
+        radius = r;
+        System.out.println("RoundGlyph.RoundGlyph(), radius = " + radius);
+    }
+    void draw(){
+        System.out.println("RoundGlyph.draw(), radius = " + radius);
+    }
+}
+class RectangleGlyph extends Glyph{
+    private int radius = 1;
+    RectangleGlyph(int r){
+        radius = r;
+        System.out.println("RectangleGlyph.RectangleGlyph(), radius = " + radius);
+    }
+    void draw(){
+        System.out.println("RectangleGlyph.draw(), radius = " + radius);
+    }
+}
+
+public class PolyConstructors {
+    public static void main(String[] args) {
+        new RoundGlyph(5);
+        new RectangleGlyph(7);
+    }
+}
