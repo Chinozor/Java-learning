792c57ce03b079bca93a65d6e00f3cd7b233ff96
Chinozor <yrikalexsin@gmail.com>
2024-11-13 11:55:00 +0300

    Создание внутренних классов

---
+++ src/main/java/p292n2/MyClass.java
@@ -0,0 +1,12 @@
+package p292n2;
+
+public class MyClass {
+    String s;
+    public MyClass(String name){
+        s = name;
+    }
+    @Override
+    public String toString() {
+        return s;
+    }
+}
---
+++ src/main/java/p292n2/Selector.java
@@ -0,0 +1,7 @@
+package p292n2;
+
+public interface Selector {
+    boolean end();
+    Object current();
+    void next();
+}
