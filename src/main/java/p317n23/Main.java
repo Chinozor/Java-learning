4f84b02347327b8d2562dcd186e9438c4fc61916
Chinozor <yrikalexsin@gmail.com>
2024-11-15 18:45:00 +0300

    ???

---
+++ src/main/java/p317n23/Main.java
@@ -0,0 +1,70 @@
+package p317n23;
+
+import p317n22.Event;
+import p317n22.GreenHouseControls;
+
+
+public class Main extends GreenHouseControls {
+
+
+    private boolean wet = false;
+    public class WetOn extends Event {
+
+        public WetOn(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            wet = true;
+        }
+
+        @Override
+        public String toString() {
+            return "Влажность включен";
+        }
+    }
+
+    public class WetOff extends Event {
+
+        public WetOff(long delayTime) {
+            super(delayTime);
+        }
+
+        @Override
+        public void action() {
+            wet = false;
+        }
+
+        @Override
+        public String toString() {
+            return "Влажность выключен";
+        }
+    }
+
+
+    public static void main(String[] args) {
+        Main gc = new Main();
+        gc.addEvent(gc.new Bell(900));
+        Event[] eventList = {
+                gc.new ThermostatNight(0),
+                gc.new LightOn(200),
+                gc.new LightOff(400),
+                gc.new WaterOn(600),
+                gc.new WaterOff(800),
+                gc.new ThermostatDay(1400),
+                gc.new FanOn(1500),
+                gc.new FanOff(1980),
+                gc.new WaterOff(2000),
+                gc.new WetOn(2222)
+        };
+
+        gc.addEvent(gc.new Restart(2500, eventList));
+
+        if (args.length == 1) {
+            gc.addEvent(new GreenHouseControls.Terminate(new Integer(args[10])));
+        }
+
+        gc.run();
+    }
+}
