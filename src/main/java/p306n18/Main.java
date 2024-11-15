2195bd36997f95125aef02011f0515d57d1fe91f
Chinozor <yrikalexsin@gmail.com>
2024-11-15 15:00:17 +0300

    Классы внутри интерфейсов

---
+++ src/main/java/p306n18/Main.java
@@ -0,0 +1,8 @@
+package p306n18;
+
+public class Main implements Outer {
+    public static void main(String[] args) {
+        Inner inner = new Inner();
+        inner.print("Получилось вроде бы");
+    }
+}
---
+++ src/main/java/p306n18/Outer.java
@@ -0,0 +1,9 @@
+package p306n18;
+
+public interface Outer {
+    class Inner{
+        void print(String s){
+            System.out.println(s);
+        }
+    }
+}
