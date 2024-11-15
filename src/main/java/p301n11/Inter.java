142a2f1d56a3a81dc45f64a333b1dc2ecb8898b2
Chinozor <yrikalexsin@gmail.com>
2024-11-15 11:23:43 +0300

    Анонимные внутренние классы

---
+++ src/main/java/p301n11/Inter.java
@@ -0,0 +1,5 @@
+package p301n11;
+
+public interface Inter {
+    void qwe();
+}
---
+++ src/main/java/p301n11/Outer.java
@@ -0,0 +1,24 @@
+package p301n11;
+
+public class Outer {
+   private Inter inter(){
+       return new Inter() {
+           @Override
+           public void qwe() {
+               System.out.println("QWE");
+           }
+           public void rty(){
+               System.out.println("RTY");
+           }
+       };
+   }
+
+    public static void main(String[] args) {
+        Outer outer = new Outer();
+        Inter inter = outer.inter();
+        inter.qwe();
+        inter.rty();
+    }
+}
+//Почему он не видит метод rty? А в номере 9 видит.
+
